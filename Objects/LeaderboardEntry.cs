3ff7f805b1b96c4b4e25c2fb00a1f405a0c2cc43
PausedMagician <75096283+PausedMagician@users.noreply.github.com>
2025-09-26 23:47:36 +0200

    Learboard fix
    If both people had drank 2 beers the one with the lower id would be on top. It is now the one who drank beer 2 first that's on top

--- Objects/LeaderboardEntry.cs
+++ Objects/LeaderboardEntry.cs
@@ -34,17 +34,21 @@ public class Leaderboard
     
     public static async Task<Leaderboard?> BeersDrankLeaderboard(DateTime date, int amount = 20)
     {
-        var entries = (await DatabaseController.Instance.db.QueryAsync<Person, long, LeaderboardEntry>(
-            "SELECT persons.id, persons.username, persons.discord_id, persons.created_at, COUNT(beers_drank) AS beers_drank " +
-            "FROM persons " +
-            "LEFT JOIN beers_drank ON persons.id = beers_drank.person_id " +
-            "WHERE DATE(beers_drank.drank_at) = DATE(@date) " +
-            "GROUP BY persons.id " +
-            "ORDER BY beers_drank DESC " +
+        var entries = (await DatabaseController.Instance.db.QueryAsync<Person, long, DateTime?, LeaderboardEntry>(
+            "SELECT p.id, p.username, p.discord_id, p.created_at, " +
+            "COUNT(bd.*) AS beers_drank, MAX(bd.drank_at) AS last_drank_at " +
+            "FROM persons p " +
+            "LEFT JOIN beers_drank bd ON p.id = bd.person_id " +
+            "WHERE DATE(bd.drank_at) = DATE(@date) " +
+            "GROUP BY p.id " +
+            "ORDER BY beers_drank DESC, last_drank_at ASC " +
             "LIMIT @amount",
-            (person, beersDrank) => new LeaderboardEntry { person = person, beers_drank = (int)beersDrank },
+            (person, beersDrank, lastDrankAt) => new LeaderboardEntry {
+                person = person,
+                beers_drank = (int)beersDrank,
+            },
             new { amount, date = date.Date },
-            splitOn: "beers_drank"
+            splitOn: "beers_drank,last_drank_at"
         )).ToList();
 
         return new Leaderboard(entries);
